2bba754196235b998009427457421505c94206a0
xamesLab <x2.0bgj@gmail.com>
2021-10-15 00:29:36 +0300

    card

--- classic/src/view/main/Grid.js
+++ classic/src/view/main/Grid.js
@@ -34,9 +34,57 @@ Ext.define("appTesting.view.main.Grid", {
       text: "Кол-во",
       dataIndex: "value",
       flex: 1,
+      renderer: function (v, m) {
+        if (+v === 0) {
+          m.style = "background-color: red";
+        }
+        return v;
+      },
     },
   ],
-  // listeners: {
-  //     select: 'onItemSelected'
-  // }
+  listeners: {
+    //   select: 'onItemSelected'
+    cellclick: function (grid, td, cellIndex, { data }, tr, rowIndex) {
+      console.log(grid.getStore());
+      var form = Ext.create("Ext.form.Panel", {
+        renderTo: Ext.getBody(),
+        bodyPadding: 5,
+        width: 350,
+
+        items: [
+          {
+            xtype: "displayfield",
+            fieldLabel: "ID:",
+            name: "theField",
+            value: `${data.id}`,
+          },
+          {
+            xtype: "displayfield",
+            fieldLabel: "Наименование:",
+            name: "theField",
+            value: `${data.caption}`,
+          },
+          {
+            xtype: "numberfield",
+            fieldLabel: "Цена:",
+            name: "theField",
+            value: `${data.price}`,
+          },
+          {
+            xtype: "numberfield",
+            fieldLabel: "Кол-во:",
+            name: "theField",
+            value: `${data.value}`,
+          },
+        ],
+      });
+      //console.log(record);
+      Ext.create("Ext.window.Window", {
+        title: `Карточка товара: ${data.name}`,
+        layout: "fit",
+
+        items: [form],
+      }).show();
+    },
+  },
 });
